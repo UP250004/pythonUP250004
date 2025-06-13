555c90d04c36203adec45f342fa2944f88ff374d
UP250004 <up250004@alumnos.upa.edu.mx>
2025-06-13 10:52:34 -0600

    .

---
+++ day06_level1.py
@@ -0,0 +1,16 @@
+####### Dia 6 --Tuples--
+## Un "Tuple" son un conjunto de datos que son inmutables (o no cambiantes), se parece a una lista, con la diferencia que no se pueden modificar
+##### Solo tienen pocos métodos 
+# tuple(): to create an empty tuple
+# count(): to count the number of a specified item in a tuple
+# index(): to find the index of a specified item in a tuple
+# operator: to join two or more tuples and to create a new 
+import os
+def pausa():
+   print("Presiona enter para continuar...")
+   input("")  # Pausa la ejecución hasta que el usuario presione Enter
+   os.system("cls")#Limpia la pantalla del sistema
+
+##### 1
+print("Creando tuple vacio...")
+##### 1
