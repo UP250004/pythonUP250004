c6cc0d543d6dfd8a073b49e216cfbf0a446de0e5
UP250004 <up250004@alumnos.upa.edu.mx>
2025-06-17 12:02:34 -0600

    .

--- day07_level1.py
+++ day07_level1.py
@@ -1,1 +1,16 @@
 #### Dia 7
+import os
+def pausa():
+   print("Presiona enter para continuar...")
+   input("")  # Pausa la ejecución hasta que el usuario presione Enter
+   os.system("cls")#Limpia la pantalla del sistema
+
+# sets
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+####### 1
+print("La longitud del set \"it_companies\" es: ",len(it_companies))
+pausa()
+####### 1
