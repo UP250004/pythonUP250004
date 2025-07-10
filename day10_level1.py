5dae9634267578312a7f7f9ba3d6d319fd023603
UP250004 <up250004@alumnos.upa.edu.mx>
2025-07-10 10:55:34 -0600

    .

---
+++ day10_level1.py
@@ -0,0 +1,32 @@
+#####Dia 10
+import os
+
+def pausa():
+   print("Presiona enter para continuar...")
+   input("")  # Pausa la ejecución hasta que el usuario presione Enter
+   os.system("cls")#Limpia la pantalla del sistema
+
+
+######### Nivel 1
+
+## 1
+# Usando for
+print("Usando el ciclo for...\n")
+for i in range(11):
+    print(i)
+pausa()
+# Usando while
+print("Usando el ciclo while...\n")
+i = 0
+while i <= 10:
+    print(i)
+    i += 1
+## 1
+pausa()
+
+## 2
+
+## 2
+
+
+######### Nivel 1
